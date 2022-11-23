b00cd9f2cc454dd2534cd05e15d8365039e2411b
fawaaaz111 <fawaztwix111@gmail.com>
2022-11-23 14:39:20 +0000

    add _print.c fiel

---
+++ test/_printf.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+ * _printf - custome version of printf
+ * @format: the output format to be printed
+ *
+ * Return: numbers of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list arg;
+	int done;
+
+	va_start (arg, format);
+	done = vfprintf (stdout, format, arg);
+	va_end (arg);
+
+	return (done);
+}
